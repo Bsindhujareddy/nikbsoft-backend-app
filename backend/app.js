7eea3ac494ece93d861dfe5ed4648393b64d39de
Bsindhujareddy <sindhuja49b@gmail.com>
2026-04-10 08:35:06 +0530

    fc

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,3 @@
-// Load .env ONLY in local environment
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
 const express = require('express');
 const cors = require('cors');
 
