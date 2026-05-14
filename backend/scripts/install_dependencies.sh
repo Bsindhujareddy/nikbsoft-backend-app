f46f190cf4a5902d9be537d512032287ddf67192
Bsindhujareddy <sindhuja94b@gmail.com>
2026-05-14 10:47:02 +0530

    fc

--- backend/scripts/install_dependencies.sh
+++ backend/scripts/install_dependencies.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-sudo chown -R ubuntu:ubuntu /home/ubuntu/backend-app
+sudo chown -R ubuntu:ubuntu /home/ubuntu/frontend-app
 
-cd /home/ubuntu/backend-app
+cd /home/ubuntu/frontend-app
+
+rm -rf node_modules
+rm -f package-lock.json
+
+npm cache clean --force
 
 npm install
